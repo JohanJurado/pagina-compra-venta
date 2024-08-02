1fdd9ddb7c97ec46370e9e73361c2d34a1bcd7b8
JohanJurado <johanjurado0103@gmail.com>
2024-08-02 09:23:10 -0500

    bd v1.0

--- compra-venta.sql
+++ compra-venta.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-10-2023 a las 14:58:45
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 02-08-2024 a las 16:21:49
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,6 +34,7 @@ CREATE TABLE `productos` (
   `foto_prod` varchar(100) NOT NULL,
   `precio_prod` int(20) NOT NULL,
   `tipo_prod` varchar(50) NOT NULL,
+  `etiqueta_producto` varchar(50) NOT NULL,
   `descuento` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -41,22 +42,22 @@ CREATE TABLE `productos` (
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id_prod`, `nombre_prod`, `id_vendedor`, `foto_prod`, `precio_prod`, `tipo_prod`, `descuento`) VALUES
-(2, 'Sofá de Jardín', 1, 'sofa-jardin.png', 300000, 'jardin', 0),
-(3, 'Mesa de Jardín', 1, 'mesa-jardin.png', 200000, 'jardin', 0),
-(4, 'Sillón de Jardín', 1, 'sillon-jardin.png', 100000, 'jardin', 0),
-(5, 'Conjunto de Jardín', 1, 'conjunto-jardin.png', 400000, 'jardin', 0),
-(6, 'Pergola Jardin', 1, 'pergola-jardin.png', 400000, 'jardin', 0),
-(7, 'Aire acondicionado Split', 2, 'aire-descuento.png', 2000000, 'aires', 5),
-(8, 'Ventilador de pie', 2, 'ventilador-descuento.png', 500000, 'aire', 10),
-(9, 'Ventilador Silencioso', 2, 'ventilador-silencioso-descuento.png', 1800000, 'aire', 10),
-(10, 'Aire acondicionado portatil', 2, 'aire-acon-portatli-descuento.png', 3000000, 'aires', 8),
-(11, 'Ventilador de techo', 2, 'ventilador-techo-descuento.png', 1500000, 'aires', 5),
-(13, 'Corta bananos', 1, 'cortador-bananos-cocina.png', 20000, 'cocina', 0),
-(15, 'Exprimidor citricos', 1, 'exprimidor-citricos-cocina2.png', 20000, 'cocina', 0),
-(16, 'Rallador con caja', 1, 'rallador-caja-cocina2.png', 70000, 'cocina', 0),
-(17, 'Batidora manual', 1, 'batidora-manual-cocina2.png', 80000, 'cocina', 0),
-(19, 'Corta manzanas', 1, 'corta-manzanas-cocina.png', 20000, 'cocina', 0);
+INSERT INTO `productos` (`id_prod`, `nombre_prod`, `id_vendedor`, `foto_prod`, `precio_prod`, `tipo_prod`, `etiqueta_producto`, `descuento`) VALUES
+(2, 'Sofá de Jardín', 1, 'sofa-jardin.png', 300000, 'jardin.', 'muebles.', 0),
+(3, 'Mesa de Jardín', 1, 'mesa-jardin.png', 200000, 'jardin.', 'muebles.', 0),
+(4, 'Sillón de Jardín', 1, 'sillon-jardin.png', 100000, 'jardin.', 'muebles.', 0),
+(5, 'Conjunto de Jardín', 1, 'conjunto-jardin.png', 400000, 'jardin.', 'otros.', 0),
+(6, 'Pergola Jardin', 1, 'pergola-jardin.png', 400000, 'jardin.', 'otros.', 0),
+(7, 'Aire acondicionado Split', 2, 'aire-descuento.png', 2000000, 'aires.', 'otros.', 5),
+(8, 'Ventilador de pie', 2, 'ventilador-descuento.png', 500000, 'aire.', 'otros.', 10),
+(9, 'Ventilador Silencioso', 2, 'ventilador-silencioso-descuento.png', 1800000, 'aire.', 'otros.', 10),
+(10, 'Aire acondicionado portatil', 2, 'aire-acon-portatli-descuento.png', 3000000, 'aires.', 'otros.', 8),
+(11, 'Ventilador de techo', 2, 'ventilador-techo-descuento.png', 1500000, 'aires.', 'otros.', 5),
+(13, 'Corta bananos', 1, 'cortador-bananos-cocina.png', 20000, 'cocina.', 'cocina.', 0),
+(15, 'Exprimidor citricos', 1, 'exprimidor-citricos-cocina2.png', 20000, 'cocina.', 'cocina.', 0),
+(16, 'Rallador con caja', 1, 'rallador-caja-cocina2.png', 70000, 'cocina.', 'cocina.', 0),
+(17, 'Batidora manual', 1, 'batidora-manual-cocina2.png', 80000, 'cocina.', 'cocina.', 0),
+(19, 'Corta manzanas', 1, 'corta-manzanas-cocina.png', 21000, 'moviles.', 'cocina.', 0);
 
 -- --------------------------------------------------------
 
@@ -69,16 +70,18 @@ CREATE TABLE `vendedor` (
   `nombre_vendedor` varchar(50) NOT NULL,
   `doc_vendedor` varchar(12) NOT NULL,
   `telefono_vendedor` varchar(12) NOT NULL,
-  `direc_vendedor` varchar(50) NOT NULL
+  `direc_vendedor` varchar(50) NOT NULL,
+  `contrasenia_vendedor` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `vendedor`
 --
 
-INSERT INTO `vendedor` (`id_vendedor`, `nombre_vendedor`, `doc_vendedor`, `telefono_vendedor`, `direc_vendedor`) VALUES
-(1, 'Valeria Martines', '2345789234', '3172893421', 'Cll 15 #2-33 '),
-(2, 'Camila Sandoval', '1099082345', '3267348234', 'Cll 10 #6-30');
+INSERT INTO `vendedor` (`id_vendedor`, `nombre_vendedor`, `doc_vendedor`, `telefono_vendedor`, `direc_vendedor`, `contrasenia_vendedor`) VALUES
+(1, 'Valeria Martines', '2345789234', '3172893421', 'Cll 15 #2-33 ', '1234'),
+(2, 'Camila Sandoval', '1099082345', '3267348234', 'Cll 10 #6-30', '1234'),
+(5, 'Angie Suarez', '1823674534', '3172382904', 'Cll 9 # 7-50 Los pinos', '1234');
 
 --
 -- Índices para tablas volcadas
@@ -111,7 +114,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `vendedor`
 --
 ALTER TABLE `vendedor`
-  MODIFY `id_vendedor` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_vendedor` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
